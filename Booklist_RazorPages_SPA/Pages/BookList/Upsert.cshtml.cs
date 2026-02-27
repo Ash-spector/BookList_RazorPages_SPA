6953ea53742020f4e1a705fb08f4b1660fec1264
Ash-spector <shuklaaniket090@gmail.com>
2026-02-27 12:44:04 +0530

    Add save  and delete button

--- Booklist_RazorPages_SPA/Pages/BookList/Upsert.cshtml.cs
+++ Booklist_RazorPages_SPA/Pages/BookList/Upsert.cshtml.cs
@@ -12,7 +12,10 @@ namespace BookList_RazorPages_SPA.Pages.BookList
         {
             _context = context;
         }
+
+        [BindProperty] // moved here; use [BindProperty(SupportsGet = true)] if you want binding on GET
         public Book Book { get; set; }
+
         public async Task <ActionResult > OnGet( int? id)
         {
             Book = new Book();//create
@@ -21,6 +24,25 @@ namespace BookList_RazorPages_SPA.Pages.BookList
             Book = await _context.Books.FindAsync(id.GetValueOrDefault());
             if (Book == null) return NotFound();
             return Page();
+        }
+        
+        public async Task<ActionResult> OnPost()
+        {
+            if (Book == null) return NotFound();
+            if (Book.Id == 0)
+                await _context.Books.AddAsync(Book);
+            else 
+                _context.Books.Update(Book);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+        }
+        public async Task <ActionResult>OnPostDelete(int id)
+        {
+            var BookInDb = await _context.Books.FindAsync(id);
+            if (BookInDb == null) return NotFound();
+            _context.Books.Remove(BookInDb);
+           await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
     }
 }
